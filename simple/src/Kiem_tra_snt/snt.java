d3edccd3a55955867dbd98fe6e1563b2e89bc4c1
huynhtv <huynhtv0311@gmail.com>
2023-06-06 12:36:34 +0700

    add check SNT

---
+++ simple/src/Kiem_tra_snt/snt.java
@@ -0,0 +1,28 @@
+package Kiem_tra_snt;
+
+import java.util.Scanner;
+
+public class snt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số muốn kiểm tra");
+        int num = scanner.nextInt();
+        boolean check = true;
+
+        if(num < 2){
+            check = false;
+
+        } else {
+            for(int i = 2; i < num; i++){
+                if(num % i == 0){
+                    check = false;
+                }
+            }
+        }
+        if(check){
+            System.out.println("Số: "+ num +" là số nguyên tố");
+        }else {
+            System.out.println("Số: "+ num +" không là số nguyên tố");
+        }
+    }
+}
