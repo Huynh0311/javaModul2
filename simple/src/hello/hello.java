d4e82cbe6f258a7065f07e12dcb4b3b644f49fd0
huynhtv <huynhtv0311@gmail.com>
2023-06-06 09:44:21 +0700

    addhello

---
+++ simple/src/hello/hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên");
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
