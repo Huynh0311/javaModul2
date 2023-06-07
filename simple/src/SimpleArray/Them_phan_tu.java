e46e2fbc6da92f646bc514f687cd6fc6139e5685
huynhtv <huynhtv0311@gmail.com>
2023-06-07 16:12:14 +0700

    add min_arr

--- simple/src/SimpleArray/Them_phan_tu.java
+++ simple/src/SimpleArray/Them_phan_tu.java
@@ -12,11 +12,14 @@ public class Them_phan_tu {
         System.out.println("Nhập vị trí thêm");
         int index = scanner.nextInt();
 
-        for (int i = array.length - 1; i > index; i--) {
-            array[i] = array[i - 1];
+        if (index <= -1 || index > array.length) {
+            System.out.println("vị trí này không có trong mảng");
+        } else {
+            for (int i = array.length - 1; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = num;
+            System.out.println("Mảng sau khi thêm là: " + Arrays.toString(array));
         }
-        array[index] = num;
-
-        System.out.println("Mảng sau khi thêm là: " + Arrays.toString(array));
     }
 }
