c9c6cdbca46e2547027b6681ab21131d85bcef0c
huynhtv <huynhtv0311@gmail.com>
2023-06-14 14:33:31 +0700

    add

---
+++ simple/src/collection_7/Interface_Comparable/Circle.java
@@ -0,0 +1,32 @@
+package collection_7.Interface_Comparable;
+
+public class Circle {
+    private double radius;
+    public Circle(){
+
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI * this.radius * 2;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +'\'' +
+                '}';
+    }
+}
+
---
+++ simple/src/collection_7/Interface_Comparable/ComparableCircle.java
@@ -0,0 +1,17 @@
+package collection_7.Interface_Comparable;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle>{
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
