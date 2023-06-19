bf638e41e7f050d2c938bb1aa0fb8067ad8e4e80
huynhtv <huynhtv0311@gmail.com>
2023-06-19 22:08:37 +0700

    add Queue_linkedList

---
+++ simple/src/collection_11/Queue_linkedList/Node.java
@@ -0,0 +1,11 @@
+package collection_11.Queue_linkedList;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
