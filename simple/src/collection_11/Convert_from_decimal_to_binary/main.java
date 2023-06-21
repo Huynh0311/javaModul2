f1e717dc5fcc25a6f574354e098b2a08064ad47f
huynhtv <huynhtv0311@gmail.com>
2023-06-21 22:26:21 +0700

    add

---
+++ simple/src/collection_11/Convert_from_decimal_to_binary/main.java
@@ -0,0 +1,26 @@
+package collection_11.Convert_from_decimal_to_binary;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class main {
+    public static void main(String[] args) {
+        Stack<Integer> integerStack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập số");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int temp = n;
+        int du;
+        do {
+            du = temp % 2;
+            integerStack.push(du);
+            temp = temp / 2;
+        }while (temp != 0);
+        int size = integerStack.size();
+        for (int i = 0; i < size; i++) {
+            System.out.print(integerStack.pop());
+        }
+    }
+}
